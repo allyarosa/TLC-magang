8d77c86f9ffd2c221c1450770f5ffb0130907204
Hamas Akif Sanie <hamasakifsanie@gmail.com>
2026-02-18 23:29:35 +0800

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
                         <h1
                             class="text-5xl md:text-6xl font-extrabold text-brandBlue leading-tight mt-4">
                             <span class="relative inlinae-block">
-                                <span class="relative z-10">Transform.</span>
+                                <span class="relative z-10">testing.</span>
                             </span>
                             <span class="relative inline-block">
                                 <span class="relative z-10">Teach.</span>
@@ -1280,15 +1280,3 @@
         {{-- Apa itu HAFECS Section --}}
     </div>
 @endsection
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
